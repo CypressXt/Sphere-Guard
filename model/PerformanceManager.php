f69eda308467dc37d0119ffaca6eec73dc6e63f2
CypressXt <cypress@cypressxt.net>
2014-01-05 02:30:54 +0100

    updated graph data

--- model/PerformanceManager.php
+++ model/PerformanceManager.php
@@ -57,7 +57,7 @@ class PerformanceManager {
 	public function getAllCPUTemp(){
 		$q = $this->_db->prepare('SELECT * FROM `performance` 
 									WHERE `fk_type` = 4 and `fk_host` = 1
-									order by date');
+									order by date limit 192');
 		$q->execute();
 
 		$chartValue ="";
