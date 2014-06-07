c90a003c8672d9b3993ae2a2034d30a9a68015bc
CypressXt <cypress@cypressxt.net>
2014-06-07 19:29:50 +0200

    updated menu links

--- view/Menu.php
+++ view/Menu.php
@@ -1,17 +1,17 @@
 <div class="col-md-3 leftMenu"> 
     <div class="MenuSeparator">General</div>
     <ul> 
-        <li class="clearer"><a href="/SphereGuard/index.php?l=apiDashboard">Home</a></li>
-        <li class="darker"><a href="#">Chart</a></li>
-        <li class="clearer"><a href="#">Statistics</a></li>
+        <li onClick="location.href = '/SphereGuard/index.php?l=apiDashboard'" class="clearer">Home</li>
+        <li onClick="location.href = '#'" class="darker">Chart</li>
+        <li onClick="location.href = '#'" class="clearer">Statistics</li>
     </ul> 
     <div class="MenuSeparator">Authentication settings</div>
     <ul> 
-        <li class="clearer"><a href="/SphereGuard/index.php?l=apiKeyManagement">API Users & API Keys</a></li>
+        <li onClick="location.href = '/SphereGuard/index.php?l=apiKeyManagement'" class="clearer">API Users & API Keys</li>
     </ul>
     <div class="MenuSeparator">My account</div>
     <ul> 
-        <li class="clearer"><a href="/SphereGuard/index.php?l=personalInfo">Personnal setting</a></li>
-        <li class="darker"><a href="/SphereGuard/index.php?l=logout">Logout</a></li>
+        <li onClick="location.href = '/SphereGuard/index.php?l=personalInfo'" class="clearer">Personnal setting</li>
+        <li onClick="location.href = '/SphereGuard/index.php?l=logout'" class="darker">Logout</li>
     </ul>
 </div>
